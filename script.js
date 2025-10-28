5a6a0f8c3457440fa869bf2e56f5dc03058332b5
hamzalafsioui <hamzalafsioui@gmail.com>
2025-10-28 19:12:46 +0100

    add check inputs in btnSave

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let modal = document.getElementById("myModal");
 // Get the element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 // get btn save
-let btnSave = document.getElementById('btn-save');
+let btnSave = document.getElementById("btn-save");
 // get btn annuler
-let btnAnnuler = document.getElementById('btn-annuler');
+let btnAnnuler = document.getElementById("btn-annuler");
 
 // add event to all active days
 const days = document.querySelectorAll(".day.active");
-
 
 // When the user clicks on <span> (x) => close the modal
 span.onclick = function () {
@@ -18,22 +17,22 @@ span.onclick = function () {
 
 // When the user clicks anywhere outside of the modal => close it
 window.onclick = function (event) {
-    // console.log(event.target);
+  // console.log(event.target);
   if (event.target == modal) {
     modal.style.display = "none";
     // console.log(event.target);
   }
 };
 // when the user clicks to annuler
-btnAnnuler.addEventListener(('click'),() =>{
-    modal.style.display = 'none';
-})
+btnAnnuler.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
 // form inputs
-const nameInput = document.getElementById('name');
-const startHourInput = document.getElementById('start-hour');
-const endHourInput = document.getElementById('end-hour');
-const selectedType = document.getElementById('type');
+const nameInput = document.getElementById("name");
+const startHourInput = document.getElementById("start-hour");
+const endHourInput = document.getElementById("end-hour");
+const selectedType = document.getElementById("type");
 
 // variable to stock selected day;
 let selectedDay = null;
@@ -45,3 +44,21 @@ days.forEach((day) => {
     modal.style.display = "block"; // When the user clicks open the modal
   });
 });
+
+// event when the user clicks on save
+btnSave.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = nameInput.value.trim();
+  const start = startHourInput.value;
+  const end = endHourInput.value;
+  const type = selectedType.value;
+
+ if(!name || !start || !end || !type){
+    alert("please fill all fields -)");
+    return;
+ }
+
+ 
+});
+
+

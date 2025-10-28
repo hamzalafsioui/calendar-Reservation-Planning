5c32ab74176f10cd7afccd8224dc46091d321075
hamzalafsioui <hamzalafsioui@gmail.com>
2025-10-28 11:17:43 +0100

    add modal to open reservation form when user click in each day

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// add event to all active days
 const days = document.querySelectorAll(".day.active");
-days.forEach(day=>{
-    day.addEventListener('click',function (){
-        console.log(day);
-    })
-})
+days.forEach((day) => {
+  day.addEventListener("click", function () {
+    // console.log(day);
+    modal.style.display = "block"; // When the user clicks open the modal
+  });
+});
+
+
+// When the user clicks on <span> (x) => close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal => close it
+window.onclick = function (event) {
+    // console.log(event.target);
+  if (event.target == modal) {
+    modal.style.display = "none";
+    // console.log(event.target);
+  }
+};
